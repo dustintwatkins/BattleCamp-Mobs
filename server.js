070234efb75d0563295e5e058c297af542fcd2ce
dustintwatkins <dustintwatkins@gmail.com>
2018-04-26 00:24:55 -0600

    working on style, database and routes are complete

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json( ));
 
 app.get('/camper_list', function(req, res){
-   console.log('GET request received!');
+   console.log('/GET request received');
 
     db.BattleCampDB.find(function(err, docs){
         console.log(docs);
@@ -21,9 +21,50 @@ app.get('/camper_list', function(req, res){
 
 });
 
-app.post('/BattleCampDB', function(req, res){
+//For adding a player to the database
+app.post('/ADDBattleCampDB', function(req, res){
     //need npm install body-parser to work
+    console.log('/POST ');
+
+    var reg_pid = req.body.pid;
+
+    //add to db iff pid dne in db. and then set values to default
+    db.BattleCampDB.update({pid:reg_pid}, {$setOnInsert:{pid:reg_pid, mob_list:[]}}, {upsert:true}, function(err, doc){
+        res.json(doc);
+    });
+
+});
+
+//For looking up a player in the database
+app.post('/LOOKUP', function(req, res){
+   console.log('/LOOKUP request');
+
+    var query = { pid:req.body};
+
+    db.BattleCampDB.find(req.body).toArray(function(err, doc) {
+        if (err) throw err;
+        console.log(doc);
+        res.json(doc)
+    });
+
+});
+
+//For adding a mob to a player
+app.post('/ADDMOB', function(req, res){
+    console.log('/ADDMOB request');
+    console.log(req.body);
+    console.log(req.body.pid);
+
+    db.BattleCampDB.update({pid:req.body.pid}, {$push:{mob_list: req.body.mob}}, {upsert:true}, function(err, doc){
+        res.json(doc);
+    });
+});
+
+app.post('/DELETEPLAYER', function(req, res){
+   console.log("/DELETEPLAYER");
    console.log(req.body);
+
+   db.BattleCampDB.remove(req.body);
 });
 
 app.listen(3000);
